appoxy/aws

commit 5c11e17f8822ee2f478af118953449fc5b083166

Added support for M/DB open source SimpleDB clone

lib/sdb/active_sdb.rb
@@ -115,6 +115,7 @@ module RightAws
             #                                                  :pool (uses a connection pool with a maximum number of connections - NOT IMPLEMENTED YET)
             #      :logger       => Logger Object        # Logger instance: logs to STDOUT if omitted
             #      :nil_representation => 'mynil'}       # interpret Ruby nil as this string value; i.e. use this string in SDB to represent Ruby nils (default is the string 'nil')
+            #      :service_endpoint	=> '/'		 # Set this to /mdb/request.mgwsi for usage with M/DB
 
             def establish_connection(aws_access_key_id=nil, aws_secret_access_key=nil, params={})
                 @connection = RightAws::SdbInterface.new(aws_access_key_id, aws_secret_access_key, params)

lib/sdb/right_sdb_interface.rb
@@ -32,6 +32,7 @@ module RightAws
     DEFAULT_HOST      = 'sdb.amazonaws.com'
     DEFAULT_PORT      = 80
     DEFAULT_PROTOCOL  = 'http'
+    DEFAULT_ENDPOINT  = '/'
     API_VERSION       = '2007-11-07'
     DEFAULT_NIL_REPRESENTATION = 'nil'
 
@@ -56,6 +57,7 @@ module RightAws
     #                                                  :pool (uses a connection pool with a maximum number of connections - NOT IMPLEMENTED YET)
     #      :logger       => Logger Object        # Logger instance: logs to STDOUT if omitted
     #      :nil_representation => 'mynil'}       # interpret Ruby nil as this string value; i.e. use this string in SDB to represent Ruby nils (default is the string 'nil')
+    #      :service_endpoint	=> '/'		 # Set this to /mdb/request.mgwsi for usage with M/DB # 
     #
     # Example:
     #
@@ -69,7 +71,8 @@ module RightAws
       init({ :name             => 'SDB',
              :default_host     => ENV['SDB_URL'] ? URI.parse(ENV['SDB_URL']).host   : DEFAULT_HOST,
              :default_port     => ENV['SDB_URL'] ? URI.parse(ENV['SDB_URL']).port   : DEFAULT_PORT,
-             :default_protocol => ENV['SDB_URL'] ? URI.parse(ENV['SDB_URL']).scheme : DEFAULT_PROTOCOL },
+             :default_protocol => ENV['SDB_URL'] ? URI.parse(ENV['SDB_URL']).scheme : DEFAULT_PROTOCOL,
+             :service_endpoint => ENV['SDB_URL'] ? URI.parse(ENV['SDB_URL']).path   : DEFAULT_ENDPOINT },
            aws_access_key_id     || ENV['AWS_ACCESS_KEY_ID'],
            aws_secret_access_key || ENV['AWS_SECRET_ACCESS_KEY'],
            params)
@@ -83,7 +86,7 @@ module RightAws
       params.delete_if {|key,value| value.nil? }
       #params_string  = params.to_a.collect{|key,val| key + "=#{CGI::escape(val.to_s)}" }.join("&")
       # prepare service data
-      service = '/'
+      service = @params[:service_endpoint]
       service_hash = {"Action"         => action,
                       "AWSAccessKeyId" => @aws_access_key_id,
                       "Version"        => API_VERSION }

commit 3d36eba1197283539db7f934747cb3f98eb74ab0

Fixed regression with merged code from hubertpicki: http://github.com/appoxy/aws/commit/5c11e17f8822ee2f478af118953449fc5b083166
Caused an infinite redirect loop.

VERSION.yml
@@ -1,5 +1,5 @@
 --- 
 :major: 1
 :minor: 11
-:patch: 35
+:patch: 36
 

aws.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{aws}
-  s.version = "1.11.35"
+  s.version = "1.11.36"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Travis Reeder", "Chad Arimura", "RightScale"]
-  s.date = %q{2009-09-25}
+  s.date = %q{2009-09-29}
   s.description = %q{AWS Ruby Library for interfacing with Amazon Web Services.}
   s.email = %q{travis@appoxy.com}
   s.extra_rdoc_files = [

lib/awsbase/right_awsbase.rb
@@ -96,9 +96,10 @@ module RightAws
             #  '%' + $1.unpack('H2' * $1.size).join('%').upcase
             #end
             e = CGI.escape(param.to_s)
+            e = e.gsub("%7E", "~")
             e = e.gsub("+", "%20")
             e = e.gsub("*", "%2A")
-            e = e.gsub("%7E", "~")
+
         end
 
 

lib/sdb/active_sdb.rb
@@ -262,6 +262,8 @@ module RightAws
                       options = args.last.is_a?(Hash) ? args.pop : {}
 #           puts 'first=' + args.first.to_s
                     case args.first
+                        when nil then
+                            raise "Invalid parameters passed to find: nil."
                         when :all   then
                             sql_select(options)
                         when :first then
@@ -358,6 +360,7 @@ module RightAws
                         # if a bunch of records was requested then return check that we found all of them
                         # and return as an array
                         unless args.size == result.size
+                            # todo: might make sense to return the array but with nil values in the slots where an item wasn't found?
                             id_list = args.map{|i| "'#{i}'"}.join(',')
                             raise ActiveSdbError.new("Couldn't find all #{name} with IDs (#{id_list}) (found #{result.size} results, but was looking for #{args.size})")
                         else

lib/sdb/right_sdb_interface.rb
@@ -32,7 +32,7 @@ module RightAws
     DEFAULT_HOST      = 'sdb.amazonaws.com'
     DEFAULT_PORT      = 443
     DEFAULT_PROTOCOL  = 'https'
-    DEFAULT_ENDPOINT  = '/'
+    DEFAULT_SERVICE  = '/'
     API_VERSION       = '2009-04-15'
     DEFAULT_NIL_REPRESENTATION = 'nil'
 
@@ -57,7 +57,7 @@ module RightAws
     #                                                  :pool (uses a connection pool with a maximum number of connections - NOT IMPLEMENTED YET)
     #      :logger       => Logger Object        # Logger instance: logs to STDOUT if omitted
     #      :nil_representation => 'mynil'}       # interpret Ruby nil as this string value; i.e. use this string in SDB to represent Ruby nils (default is the string 'nil')
-    #      :service_endpoint	=> '/'		 # Set this to /mdb/request.mgwsi for usage with M/DB # 
+    #      :service      => '/'	                 # Set this to /mdb/request.mgwsi for usage with M/DB #
     #
     # Example:
     #
@@ -72,7 +72,8 @@ module RightAws
              :default_host     => ENV['SDB_URL'] ? URI.parse(ENV['SDB_URL']).host   : DEFAULT_HOST,
              :default_port     => ENV['SDB_URL'] ? URI.parse(ENV['SDB_URL']).port   : DEFAULT_PORT,
              :default_protocol => ENV['SDB_URL'] ? URI.parse(ENV['SDB_URL']).scheme : DEFAULT_PROTOCOL,
-             :service_endpoint => ENV['SDB_URL'] ? URI.parse(ENV['SDB_URL']).path   : DEFAULT_ENDPOINT },
+             :default_service  => ENV['SDB_URL'] ? URI.parse(ENV['SDB_URL']).path   : DEFAULT_SERVICE },
+#             :service_endpoint => ENV['SDB_URL'] ? URI.parse(ENV['SDB_URL']).path   : DEFAULT_ENDPOINT },
            aws_access_key_id     || ENV['AWS_ACCESS_KEY_ID'],
            aws_secret_access_key || ENV['AWS_SECRET_ACCESS_KEY'],
            params)
@@ -86,12 +87,13 @@ module RightAws
       params.delete_if {|key,value| value.nil? }
       #params_string  = params.to_a.collect{|key,val| key + "=#{CGI::escape(val.to_s)}" }.join("&")
       # prepare service data
-      service = @params[:service_endpoint]
+        service = @params[:service]
+#      puts 'service=' + service.to_s
       service_hash = {"Action"         => action,
                       "AWSAccessKeyId" => @aws_access_key_id,
                       "Version"        => API_VERSION }
       service_hash.update(params)
-      service_params = signed_service_params(@aws_secret_access_key, service_hash, :get, @params[:server], service)
+      service_params = signed_service_params(@aws_secret_access_key, service_hash, :get, @params[:server], @params[:service])
       #
       # use POST method if the length of the query string is too large
       # see http://docs.amazonwebservices.com/AmazonSimpleDB/2007-11-07/DeveloperGuide/MakingRESTRequests.html

test/sdb/test_active_sdb.rb
@@ -1,4 +1,5 @@
 require File.dirname(__FILE__) + '/test_helper.rb'
+require '../test_credentials'
 
 class TestSdb < Test::Unit::TestCase
   

test/test_credentials.rb
@@ -27,7 +27,8 @@ class TestCredentials
   def self.get_credentials
     #Dir.chdir do
       begin
-        Dir.chdir('.rightscale') do 
+          puts Dir.chdir
+        Dir.chdir('.amazon') do
           credentials = YAML::load(File.open("testcredentials.yml"))
           puts credentials.inspect
             self.aws_access_key_id = credentials["access_key"]
@@ -35,7 +36,7 @@ class TestCredentials
             puts 'akey=' + self.aws_access_key_id
         end
       rescue Exception => e
-        puts "Couldn't chdir to ~/.rightscale: #{e.message}"
+        puts "Couldn't chdir to ~/.amazon: #{e.message}"
           raise e
       end
     #end