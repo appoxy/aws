026cef9226ca48bfc1980edda4c6a3548bb134fa
Michal Fojtik <mfojtik@redhat.com>
2010-11-10 16:39:41 +0100

    Added support for t1.micro instance_type

--- lib/ec2/right_ec2.rb
+++ lib/ec2/right_ec2.rb
@@ -81,7 +81,7 @@ module Aws
     # Amazon EC2 Instance Types : http://www.amazon.com/b?ie=UTF8&node=370375011
     # Default EC2 instance type (platform)
     DEFAULT_INSTANCE_TYPE   =  'm1.small'
-    INSTANCE_TYPES          = ['m1.small','c1.medium','m1.large','m1.xlarge','c1.xlarge']
+    INSTANCE_TYPES          = ['t1.micro', 'm1.small','c1.medium','m1.large','m1.xlarge','c1.xlarge']
 
     @@bench = AwsBenchmarkingBlock.new
     def self.bench_xml
