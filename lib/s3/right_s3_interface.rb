115b5bc99c6aaac4ac2c158b33ce17d17296937b
Michal Fojtik <mfojtik@redhat.com>
2010-11-29 12:08:27 +0100

    Fixed Location for S3 buckets when want to use us-west-1 or ap-southeast-1

--- lib/s3/right_s3_interface.rb
+++ lib/s3/right_s3_interface.rb
@@ -204,7 +204,9 @@ module Aws
         def create_bucket(bucket, headers={})
             data     = nil
             unless headers[:location].blank?
-                data = "<CreateBucketConfiguration><LocationConstraint>#{headers[:location].to_s.upcase}</LocationConstraint></CreateBucketConfiguration>"
+                location = "#{headers[:location]}"
+                location.upcase! if location == 'eu'
+                data = "<CreateBucketConfiguration><LocationConstraint>#{location}</LocationConstraint></CreateBucketConfiguration>"
             end
             req_hash = generate_rest_request('PUT', headers.merge(:url=>bucket, :data => data))
             request_info(req_hash, RightHttp2xxParser.new)
