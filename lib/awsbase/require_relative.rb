341da9bac80cdcd74cc753d2c51e7c6efd8ff5c3
Travis Reeder <travis@appoxy.com>
2011-01-17 13:54:15 -0800

    Fixed this so it doesn't override the real method in 1.9.2 and uses the same acode as 1.9.2

--- lib/awsbase/require_relative.rb
+++ lib/awsbase/require_relative.rb
@@ -1,17 +1,11 @@
-unless respond_to?(:require_relative)
-  class Object
-    # require_relative was introduced in 1.9.2. This makes it
-    # available to younger rubies.
-    # From: http://stackoverflow.com/questions/4333286/ruby-require-vs-require-relative-best-practice-to-workaround-running-in-both-r/4338241#4338241
-    def require_relative(relative_feature)
-      c = caller.first
-      fail "Can't parse #{c}" unless c.rindex(/:\d+(:in `.*')?$/)
-      file = $`
-      if /\A\((.*)\)/ =~ file # eval, etc.
-        raise LoadError, "require_relative is called in #{$1}"
-      end
-      absolute = File.expand_path(relative_feature, File.dirname(file))
-      require absolute
+# require_relative was introduced in 1.9.2. This makes it
+# available to younger rubies.
+# From: http://stackoverflow.com/questions/4333286/ruby-require-vs-require-relative-best-practice-to-workaround-running-in-both-r/4338241#4338241
+unless Kernel.respond_to?(:require_relative)
+  module Kernel
+    def require_relative(path)
+      puts 'IN NEW REQUIRE_RELATIVE ' + path.to_s
+      require File.join(File.dirname(caller[0]), path.to_str)
     end
   end
 end
