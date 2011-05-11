2e450cc834a004c5ce7579b3beacf7d33e762802
marios <marios@redhat.com>
2011-05-12 00:31:58 +0300

    Updates AuthorizeSecurityGroupIngress and RevokeSecurityGroupIngress to latest API version - allows passing more than one IP address or source group (or mix of both) for the rule being specified

--- lib/ec2/ec2.rb
+++ lib/ec2/ec2.rb
@@ -827,67 +827,42 @@ module Aws
       on_exception
     end
 
-    # Authorize named ingress for security group. Allows instances that are member of someone
-    # else's security group to open connections to instances in my group.
+    # Authorize OR Revoke ingress for security group, depending on the value of the 'action' parameter.
+    # If you 'authorize' then you allow instances that are member of some other 
+    # security groups, or some range of ip addresses to open connections to instances in 
+    # my group. Can specify an array of ip addresses, source groups or mix of both in a single rule:
     #
-    #  ec2.authorize_security_group_named_ingress('my_awesome_group', '7011-0219-8268', 'their_group_name') #=> true
+    # ec2.manage_security_group_ingress('authorize', 'new_firewall', 80, 80, 'tcp', ['192.168.0.1/32', '10.0.0.1/24'],
+    #  				   [{'group_name'=>'default', 'owner'=>'297467797945'}, {'group_name'=>'test', 'owner'=>'123456789012'}])
     #
-    def authorize_security_group_named_ingress(name, owner, group)
-      link = generate_request("AuthorizeSecurityGroupIngress",
-                              'GroupName'                  => name.to_s,
-                              'SourceSecurityGroupName'    => group.to_s,
-                              'SourceSecurityGroupOwnerId' => owner.to_s.gsub(/-/, ''))
-      request_info(link, RightBoolResponseParser.new(:logger => @logger))
-    rescue Exception
-      on_exception
-    end
-
-    # Revoke named ingress for security group.
+    # ec2.manage_security_group_ingress('new_firewall', 0, 1000, 'udp', 'revoke', [], 
+    #    												 [{'group_name'=>'default', 'owner'=>'123456789012'}])
     #
-    #  ec2.revoke_security_group_named_ingress('my_awesome_group', aws_user_id, 'another_group_name') #=> true
+    # ec2.manage_security_group_ingress('new_firewall', 0, 1000, 'udp', 'authorize', ['0.0.0.0/0']) 
     #
-    def revoke_security_group_named_ingress(name, owner, group)
-      link = generate_request("RevokeSecurityGroupIngress",
-                              'GroupName'                  => name.to_s,
-                              'SourceSecurityGroupName'    => group.to_s,
-                              'SourceSecurityGroupOwnerId' => owner.to_s.gsub(/-/, ''))
-      request_info(link, RightBoolResponseParser.new(:logger => @logger))
-    rescue Exception
-      on_exception
-    end
-
-    # Add permission to a security group. Returns +true+ or an exception. +protocol+ is one of :'tcp'|'udp'|'icmp'.
+    # Similarly, if you specify 'revoke' as the action parameter then you will remove the specified 
+    # source ip addresses or source groups from access to instances in the named group:
     #
-    #  ec2.authorize_security_group_IP_ingress('my_awesome_group', 80, 82, 'udp', '192.168.1.0/8') #=> true
-    #  ec2.authorize_security_group_IP_ingress('my_awesome_group', -1, -1, 'icmp') #=> true
-    #
-    def authorize_security_group_IP_ingress(name, from_port, to_port, protocol='tcp', cidr_ip='0.0.0.0/0')
-      link = generate_request("AuthorizeSecurityGroupIngress",
-                              'GroupName'  => name.to_s,
-                              'IpProtocol' => protocol.to_s,
-                              'FromPort'   => from_port.to_s,
-                              'ToPort'     => to_port.to_s,
-                              'CidrIp'     => cidr_ip.to_s)
-      request_info(link, RightBoolResponseParser.new(:logger => @logger))
-    rescue Exception
-      on_exception
-    end
-
-    # Remove permission from a security group. Returns +true+ or an exception. +protocol+ is one of :'tcp'|'udp'|'icmp' ('tcp' is default).
-    #
-    #  ec2.revoke_security_group_IP_ingress('my_awesome_group', 80, 82, 'udp', '192.168.1.0/8') #=> true
-    #
-    def revoke_security_group_IP_ingress(name, from_port, to_port, protocol='tcp', cidr_ip='0.0.0.0/0')
-      link = generate_request("RevokeSecurityGroupIngress",
-                              'GroupName'  => name.to_s,
-                              'IpProtocol' => protocol.to_s,
-                              'FromPort'   => from_port.to_s,
-                              'ToPort'     => to_port.to_s,
-                              'CidrIp'     => cidr_ip.to_s)
-      request_info(link, RightBoolResponseParser.new(:logger => @logger))
-    rescue Exception
-      on_exception
-    end
+		def manage_security_group_ingress(name, from_port, to_port, protocol, action, source_ip_ranges, source_groups = [])
+			call_params = {	'GroupName'	 => name.to_s,
+											'IpPermissions.1.IpProtocol' => protocol.to_s,
+                     	'IpPermissions.1.FromPort'   => from_port.to_s,
+                     	'IpPermissions.1.ToPort'     => to_port.to_s	}
+			source_ip_ranges.each_index do |i| 
+				call_params.merge!({"IpPermissions.1.IpRanges.#{i+1}.CidrIp" => source_ip_ranges[i].to_s})
+			end
+			source_groups.each_index do |i|
+				call_params.merge!({"IpPermissions.1.Groups.#{i+1}.GroupName" => source_groups[i]['group_name'].to_s, 
+														"IpPermissions.1.Groups.#{i+1}.UserId"=> source_groups[i]['owner'].to_s.gsub(/-/,'')})
+			end
+		  unless ['Authorize', 'Revoke'].include?(action.capitalize)
+				 raise AwsError.new("Invalid action #{action} - must be one of \'Authorize\' or \'Revoke\'")
+      end
+			link = generate_request("#{action.capitalize}SecurityGroupIngress", call_params)
+			request_info(link, RightBoolResponseParser.new(:logger => @logger))
+			rescue Exception
+				on_exception	
+		end
 
     #-----------------------------------------------------------------
     #      Keys
